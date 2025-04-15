c45c1a88bddc5ddca9979a977f96a5beaa000c2c
mibdrt-dale <dkr1964dale@gmail.com>
2025-04-15 15:32:29 -0700

    merge
    all merge

--- WorkerService1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.Intrinsics.X86;
-using static System.Collections.Immutable.ImmutableArray<Combiner>;
-
-Builder.Services.AddHostedService<Worker>();
-
-var host = Avx512BW.Build();
-host.Run();
